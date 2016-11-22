213808c45b59eac93c0db0c50e073238cb284151
Minorias <pavel_polinetskyy@hotmail.com>
2016-11-22 19:29:34 +0100

    removed print

--- parser.py
+++ parser.py
@@ -27,10 +27,6 @@ class InputParser:
 
             node_1.add_debtor(node_2, amount)
 
-        # for key,item in self.node_dictionary.items():
-        #     print(key, item.name, [[debtor[0].name,debtor[1]] for debtor in item.debtors])
-
-
     def get_node(self,name):
         try:
             return self.node_dictionary[name]
