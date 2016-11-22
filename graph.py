c21c689840a64d70376df19521d031eca37d3db0
GR4W <grisa181@yahoo.com>
2016-11-22 20:52:54 +0000

    Merge again blyad

--- graph.py
+++ graph.py
@@ -18,19 +18,10 @@ class Node:
 
 
 class Graph:
-<<<<<<< Updated upstream
     def __init__(self, node_list):
         self.nodes = node_list
 
 
     def simplify_debts(self):
         pass
-=======
-    def __init__(self, graph_dictionary):
-        self.nodes = graph_dictionary
 
-    def graph_simplify(self):
-    	pass
-
-    #def findsocialHubs
->>>>>>> Stashed changes
