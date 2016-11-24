afbba45fc1adcc3d6152ca15c658ca75d126c5f0
gr4w <grisa181@yahoo.com>
2016-11-24 20:25:05 +0000

    probably failed to find communities

--- graph.py
+++ graph.py
@@ -23,7 +23,7 @@ class Node:
         this.debt_amounts[index] = new_amount
 
     def __eq__(self, other):
-        if isinstance(other, str)
+        if isinstance(other, str):
             return self.name == other
         elif isinstance(other, Node):
             return self.name == other.name
@@ -31,14 +31,28 @@ class Node:
             raise NotImplementedError
 
 class Graph:
-    def __init__(self, node_list):
-        self.nodes = node_list
+	def __init__(self, node_list):
+		self.nodes = node_list
 
 
-    def simplify_debts(self):
-        pass
+	def simplify_debts(self):
+		pass
 
 
-    def find_communities(self):
-    	
-    	for node in self.nodes:
+	def find_communities(self):
+		communityList = []
+
+		for root in self.nodes:
+			currentCommunity = []
+			currentCommunity.append(root)
+			for node in self.nodes:
+				if node in root.get_debtors() or node in root.get_creditors():
+					currentCommunity.append(node)
+					
+			if len(communityList) > 0:
+				for community in communityList:
+					if set(currentCommunity) != set(community):			
+						communityList.append(currentCommunity)
+
+		return communityList
+
