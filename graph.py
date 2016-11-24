3c1cdf5e7cf17f106a5e8513f22cc24679728a37
Minorias <pavel_polinetskyy@hotmail.com>
2016-11-24 18:24:05 +0000

    Added creditors

--- graph.py
+++ graph.py
@@ -2,20 +2,28 @@ class Node:
     def __init__(self,name):
         self.name = name
         self.debtors = []
+        self.creditors = []
+        self.debt_amounts = []
 
     def add_debtor(self, debtor, amount):
-        self.debtors.append([debtor, amount])
+        self.debtors.append(debtor)
+        self.debt_amounts.append(amount)
+        
+    def add_creditor(self,creditor):
+    	self.creditors.append(creditor)
 
     def get_debtors(self):
         return self.debtors
 
+    def get_creditors(self):
+    	return self.creditors
+
     def change_debtor_amount(self, debtor, new_amount):
-        index = this.debtors.indexgit (debtor)
+        index = this.debtors.index(debtor)
         this.debt_amounts[index] = new_amount
 
     def __eq__(self, other_name):
         return self.name == other_name
-
 
 class Graph:
     def __init__(self, node_list):
