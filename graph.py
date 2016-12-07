8faaa4332fdf5d3c527936947f33899d2e425d54
Minorias <pavel_polinetskyy@hotmail.com>
2016-12-07 13:09:03 +0100

    [add] Cycle enumeration, working Johnson's algorithm

--- graph.py
+++ graph.py
@@ -1,5 +1,6 @@
 from itertools import chain
 from math import inf as infinity
+from copy import copy
 
 class Node:
     def __init__(self,name):
@@ -52,7 +53,6 @@ class SCCs_Tarjan:
     def __init__(self, node_list, removed_set):
         self.nodes = node_list
         self.removed_set = removed_set
-
         self.overall_id = 0
         self.treated_nodes = {node: -1 for node in self.nodes if node not in self.removed_set}
         self.lowest_backedge = {node: 0 for node in self.nodes if node not in self.removed_set}
@@ -73,7 +73,7 @@ class SCCs_Tarjan:
         self.node_stack.append(curr_node)
 
         for child in curr_node.get_creditors():
-            if child not in self.removed_set:
+            if (child in self.nodes) and (child not in self.removed_set):
                 if self.treated_nodes[child] == -1:
                     self.explore(child)
                 my_min = min(my_min, self.lowest_backedge[child])
@@ -83,14 +83,90 @@ class SCCs_Tarjan:
         else:
             current_scc = []
 
-            popped_node = self.node_stack.pop()
-            current_scc.append(popped_node)
-            while popped_node != curr_node:
+            condition = True
+            while condition:
                 popped_node = self.node_stack.pop()
+                if popped_node == curr_node:
+                    condition = False
                 current_scc.append(popped_node)
                 self.lowest_backedge[popped_node] = infinity
 
             self.sccs.append(current_scc)
+
+
+class Cycles_Johnson:
+    def __init__(self, node_list):
+        self.nodes = node_list
+        self.node_stack = []
+
+        self.removed_set = set()
+        self.blocked_set = set()
+        self.blocked_map = {}
+        self.allcycles = []
+
+    def get_cycles(self):
+        # List of all the strongly connected components of the graph
+        all_sccs = SCCs_Tarjan(self.nodes, self.removed_set).find_sccs()
+        # We look at each strongly connected subgraph
+        i = 0
+        while i < len(all_sccs):
+            current_scc = all_sccs[i]
+            while len(current_scc) > 1:
+                starting_node = current_scc[0]
+                                                               #convert it to a set for faster lookup in the future
+                self.find_cycles(starting_node, starting_node, set(current_scc))
+                self.removed_set.add(starting_node)
+                # We remove the starting node and recalculate the strongly connected components
+                #   if this happens to break the current scc into several we take the first one
+                #   to work on and add the rest to the end of the list of all the other sccs
+                possibly_many_sccs = SCCs_Tarjan(current_scc, self.removed_set).find_sccs()
+                current_scc = possibly_many_sccs[0]
+                all_sccs.extend(possibly_many_sccs[1:])
+
+                self.blocked_map.clear()
+                self.blocked_set.clear()
+
+            i+=1
+            self.removed_set.clear()
+
+        return self.allcycles
+
+    def find_cycles(self, starting_node, current_node, current_scc):
+        found_cycle = False
+        self.node_stack.append(current_node)
+        self.blocked_set.add(current_node)
+        for child in current_node.get_creditors():
+            if child not in self.removed_set and child in current_scc:
+                if child == starting_node: #We have completed a cycle
+                    self.allcycles.append(self.node_stack[::-1])
+                    found_cycle = True
+
+                elif child not in self.blocked_set:
+                    found_cycle = self.find_cycles(starting_node, child, current_scc) or found_cycle
+
+        if found_cycle:
+            self.unblock(current_node)
+        else:
+            for child in current_node.get_creditors():
+                if child not in self.removed_set and child in current_scc:
+                    if child in self.blocked_map:
+                        self.blocked_map[child].append(current_node)
+                    else:
+                        self.blocked_map[child] = [current_node]
+
+        self.node_stack.pop()
+
+        return found_cycle
+
+    def unblock(self, node):
+        # print(node, self.blocked_map, self.blocked_set)
+        self.blocked_set.remove(node)
+        if node in self.blocked_map:
+            for other_node in self.blocked_map[node]:
+                if other_node in self.blocked_set:
+                    self.unblock(other_node)
+            self.blocked_map.pop(node)
+
 
 class Graph:
     def __init__(self, node_list):
@@ -99,6 +175,10 @@ class Graph:
     def find_sccs(self):
         tarjan = SCCs_Tarjan(self.nodes, set())
         return tarjan.find_sccs()
+
+    def find_cycles(self):
+        cycles = Cycles_Johnson(self.nodes).get_cycles()
+        return cycles
 
     def simplify_debts(self):
         pass
--- main.py
+++ main.py
@@ -3,4 +3,6 @@ from graph import Graph, Node
 
 
 graph = Graph(InputParser("data.txt").graph_output())
-print(graph.find_communities())
+print("Communities: ",graph.find_communities())
+print("Strongly Connected Components: ",graph.find_sccs())
+print("Cycles: ", graph.find_cycles())
